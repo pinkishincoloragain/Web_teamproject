bae47e1c25a80d62f84d406b96feaacf43f39d3f
pinkishincoloragain <smb1103@gmail.com>
2021-05-27 04:16:03 +0900

    made Hello pange

---
+++ blog/src/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome(props) {
+  return <h1>let's get it started {props.name}</h1>;
+}
+
+export default Welcome;
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App.js";
+import Welcome from "./Welcome.js";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const welcomer = <Welcome name="3!3!마3!" />;
+ReactDOM.render(welcomer, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

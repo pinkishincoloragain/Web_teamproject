3d8681a5963d477c2bbd32fa847cd6f5f723031c
DDAGUE <31721315+DDAGUE@users.noreply.github.com>
2021-05-29 20:01:43 +0900

    Update getID.js

--- blog/src/getID.js
+++ blog/src/getID.js
@@ -1,12 +1,16 @@
 var req = new XMLHttpRequest();
 
 function getID(word){
+
+    var data;
+    var res;
+
     // 영상 검색에 필요한 변수
     var optionParams={
         q: word,
         part: "snippet",
         key: "AIzaSyCUIDAQFFfLpwYwJmogvgadNFq7kV4yhvg", // API key value
-        maxResults: 2
+        maxResults: 1
     }
     // 한글은 uri encoding 해야함.
     optionParams.q = encodeURI(optionParams.q);
@@ -20,7 +24,12 @@ function getID(word){
 
     req.open('GET', url);
     req.onload = () => {
-        console.log(req.response);
+        data = JSON.parse(req.response);
+        var idx = String(Object.keys(data)[5])
+        var res = data[idx]
+        res = res[0].id.videoId;
     };
     req.send();
+
+    return res;
 }

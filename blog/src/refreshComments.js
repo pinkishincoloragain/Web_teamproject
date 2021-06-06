ce20ea22a71a18d94407929e359560ef8916ff91
choi-jaewon <sodlapdlf13@gmail.com>
2021-06-07 03:25:22 +0900

    repair RefreshComments

--- blog/src/refreshComments.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-function RefreshComments(props) {
-  // const [address, setAddress] = useState("///당신의 위치를 고르세요!");
-
-  return <div>{props.address}</div>;
-}
-
-export default RefreshComments;

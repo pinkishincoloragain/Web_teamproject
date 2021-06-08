2397cb8f1d8e9e2685651e4b481f1b3688fd7dc4
choi-jaewon <sodlapdlf13@gmail.com>
2021-06-08 19:14:41 +0900

    change to class

---
+++ blog/src/Comments.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css"
+
+class Comments extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={result:[]};
+    }
+
+    
+
+    deleteComment(){
+
+    }
+
+    render() {
+        return(
+          <div>
+
+          </div>
+        );
+    }
+}
+
+export default Comments;

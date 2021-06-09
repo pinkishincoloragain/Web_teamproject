5923ad660dac5441a9b5c8663c4b43d1048ee5c3
choi-jaewon <sodlapdlf13@gmail.com>
2021-06-09 22:59:04 +0900

    add scrollComments

--- blog/src/Comments.js
+++ blog/src/Comments.js
@@ -2,25 +2,24 @@ import React, {useState} from "react";
 import "./App.css"
 import axios from "axios";
 
-function deleteComment(wid, wpass){
-    let inputPass = prompt("비밀번호를 입력하세요");
-    console.log("delete()")
-    if (inputPass === wpass) {
-        let response = fetch("/deleteComment.php?wid=" + wid);
-        alert("글이 삭제되었습니다!");
-    } else {
-        alert("비밀번호가 틀렸습니다!");
-    }
-}
-
 class CommentsInfo extends React.Component{
+    deleteComment = (wid, wpass) => {
+        let inputPass = prompt("비밀번호를 입력하세요");
+        console.log("delete()")
+        if (inputPass === wpass) {
+            let response = fetch("/deleteComment.php?wid=" + wid);
+            alert("글이 삭제되었습니다!");
+        } else {
+            alert("비밀번호가 틀렸습니다!");
+        }
+    }
     render() {
         return(
             <div>
                 <span>Name : {this.props.comments['wname']}</span><br/>
                 <span>Content : {this.props.comments['content']}</span><br/>
                 <span>Date : {this.props.comments['wdate']}</span><br/>
-                <span><button onClick={()=>deleteComment(this.props.comments['wid'], this.props.comments['wpass'])}>ㅡ</button></span>
+                <span><button onChange={Comments.handleChange} onClick={() => this.deleteComment(this.props.comments['wid'], this.props.comments['wpass'])}>ㅡ</button></span>
                 <hr/>
             </div>
         );
@@ -31,6 +30,7 @@ class Comments extends React.Component{
     constructor(props) {
         super(props);
         this.state={
+            w3: '',
             wname: '',
             wpass: '',
             content: '',
@@ -41,6 +41,12 @@ class Comments extends React.Component{
     handleChange = e =>{
         this.setState({
             [e.target.name]: e.target.value
+        })
+    }
+
+    handleW3 = e => {
+        this.setState({
+            w3: this.props.address
         })
     }
 
@@ -83,13 +89,16 @@ class Comments extends React.Component{
 
         return(
           <div className={"ContentBox"} style={{backgroundColor: this.props.color}}>
-              <p>{this.props.address}</p>
+              <p onChange={this.handleW3}>{this.state.w3}</p>
               <form onSubmit={this.handleAdd} id={"addForm"}>
                   <table border={5}  className="one_table" >
                       <tr>
                           <td>Name</td>
                           <td>
                               <input type={"text"} name={"wname"} value={this.state.wname} onChange={this.handleChange} className={"TextBox"}/>
+                          </td>
+                          <td rowSpan={3}>
+                              <input type={"submit"} value={"+"}/>
                           </td>
                       </tr>
                       <tr>
@@ -103,19 +112,16 @@ class Comments extends React.Component{
                               <textarea placeholder={"내용을 입력하세요"} name={"content"} value={this.state.content} onChange={this.handleChange} className={"InputBox"}/>
                           </td>
                       </tr>
-                      <tr>
-                          <td colSpan={4} >
-                              <input type={"submit"} value={"+"}/>
-                          </td>
-                      </tr>
                   </table>
               </form>
               <br/>
-              Comments
-              <button onClick={this.handleShow} onChange={this.handleChange}>
+              <h3>Comments</h3>
+              <button className={"refreshButton"} onClick={this.handleShow} onChange={this.handleChange}>
                   refresh
               </button>
-              {mapToComponent(this.state.comments)}
+              <div className={"scrollComments"}>
+                  {mapToComponent(this.state.comments)}
+              </div>
           </div>
         );
     }
